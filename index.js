0f02c666c4da5bcc520d04427708f958eef0c1b6
CarlosColindres <70364536+CarlosColindres@users.noreply.github.com>
2020-09-15 16:58:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -336,11 +336,15 @@ function getHTML(/* Code here */){
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
-
-    /* Code here */
-
-  }
+function randomize(arr){
+  let randomArr = [];
+  for (i = arr.length - 1; i >= 0; i--) {
+      let num = Math.floor(Math.random() * arr.length)
+      randomArr.push(arr[num]);
+      arr.splice(num,1)
+  }         
+  return randomArr;
+}
 
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
